0390a395300b95a74c53ad6882bac7011476cbfc
Noah Vêri Franco <noahvf16@outlook.com>
2025-01-26 15:43:46 -0300

    Create the controller class

---
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/Controller.java
@@ -0,0 +1,19 @@
+package dev.Java10x.Projeto1SpringCadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+        @GetMapping("/boa")
+        @ResponseBody
+        public String boasVindas(){
+            return "Bem vindo ao sistema de cadastro de clientes!";
+        }
+
+
+}
--- src/test/java/dev/Java10x/Projeto1SpringCadastro/Projeto1SpringCadastroApplicationTests.java
+++ src/test/java/dev/Java10x/Projeto1SpringCadastro/Projeto1SpringCadastroApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Projeto1SpringCadastroApplicationTests {
 
+
 	@Test
 	void contextLoads() {
 	}
