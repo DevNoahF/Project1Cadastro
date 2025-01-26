e637053ee78d969e1803e7590f75016541aa4be8
Noah Vêri Franco <noahvf16@outlook.com>
2025-01-25 22:31:05 -0300

    first commit

---
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/Projeto1SpringCadastroApplication.java
@@ -0,0 +1,13 @@
+package dev.Java10x.Projeto1SpringCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto1SpringCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1SpringCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Java10x/Projeto1SpringCadastro/Projeto1SpringCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Java10x.Projeto1SpringCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1SpringCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
