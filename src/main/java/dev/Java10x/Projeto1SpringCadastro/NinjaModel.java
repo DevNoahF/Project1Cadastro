8903d818c0bd8c8efd7039cd976b4ab179a0f5aa
Noah Vêri Franco <noahvf16@outlook.com>
2025-01-26 16:18:04 -0300

    add NinjaModel for SQL and modify the Controller for the new Ninja entity

--- src/main/java/dev/Java10x/Projeto1SpringCadastro/NinjaModel.java
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/NinjaModel.java
@@ -1,4 +1,58 @@
 package dev.Java10x.Projeto1SpringCadastro;
+import jakarta.persistence.*;
 
+// Entity ele transforma uma classe em uma entidade do banco de dados (DB)
+// JPA -> Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
 public class NinjaModel {
+
+    // Id é a chave primária
+    @Id // Indica que é a chave primária da tabela do banco de dados (DB)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera um valor automaticamente (passa o jeito que vc quer que gere o ID)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
