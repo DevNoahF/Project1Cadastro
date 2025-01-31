9a83b25dc63d7f27850245c0156b61fcfb7a6402
Noah Vêri Franco <146153343+DevNoahF@users.noreply.github.com>
2025-01-31 11:55:21 -0300

    edit Model

--- src/main/java/dev/Java10x/Projeto1SpringCadastro/Missoes/MissoesModel.java
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.Java10x.Projeto1SpringCadastro.Missoes;
 
 import dev.Java10x.Projeto1SpringCadastro.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor // faz com que seja criado um construtor sem argumentos
+@AllArgsConstructor // faz com que seja criado um cosntrutor com argumentos
+@Data // Cria todos os getters and setters
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/Java10x/Projeto1SpringCadastro/Ninjas/NinjaModel.java
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/Ninjas/NinjaModel.java
@@ -1,11 +1,17 @@
 package dev.Java10x.Projeto1SpringCadastro.Ninjas;
 import dev.Java10x.Projeto1SpringCadastro.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Entity ele transforma uma classe em uma entidade do banco de dados (DB)
 // JPA -> Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor // faz com que seja criado um construtor sem argumentos
+@AllArgsConstructor // faz com que seja criado um cosntrutor com argumentos
+@Data // Cria todos os getters and setters
 public class NinjaModel {
 
     // OneToOne -> Um para um
@@ -23,45 +29,6 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Indica que é uma chave estrangeira, é pra ser utilizada sempre que usar o MANYTOONE
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
