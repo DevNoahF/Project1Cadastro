e7c6ae2ba55e25eadb26ea6aa736f89a91111c77
Noah Vêri Franco <146153343+DevNoahF@users.noreply.github.com>
2025-02-24 19:50:49 -0300

    feat: update controller with MockData

--- src/main/java/dev/Java10x/Projeto1SpringCadastro/Ninjas/NinjaController.java
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/Ninjas/NinjaController.java
@@ -1,9 +1,6 @@
 package dev.Java10x.Projeto1SpringCadastro.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -16,4 +13,33 @@ public class NinjaController {
         }
 
 
+        // CRUD
+
+    // adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNija(){
+            return "Ninja criado com sucesso!";
+    }
+
+    // mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas(){
+            return "Ninjas criadas!";
+    }
+
+    // mostrar ninjas por ID (READ)
+    @GetMapping("/TodosID")
+    public String mostrarTodosNinjasId(){
+            return "Mostrar Ninja por ID!";
+    }
+    //alterar dados do ninja(UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinja(){
+            return "Ninja alterado com sucesso!";
+    }
+    // deletar ninja(DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinja(){
+            return "Ninja deletado com sucesso!";
+    }
 }
