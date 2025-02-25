7ec7da02d4bf9b723cc4a80276ac59a954ba8857
Noah Vêri Franco <146153343+DevNoahF@users.noreply.github.com>
2025-02-24 21:23:49 -0300

    feat: add crate logs with mock

--- src/main/java/dev/Java10x/Projeto1SpringCadastro/Missoes/MissoesController.java
+++ src/main/java/dev/Java10x/Projeto1SpringCadastro/Missoes/MissoesController.java
@@ -1,4 +1,36 @@
 package dev.Java10x.Projeto1SpringCadastro.Missoes;
 
+ // LOCALHOST:8080/deletar/1 (quando quiser deletar pelo id)
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("missoes")
 public class MissoesController {
+
+    // GET -- Mandar uma requisição para mostrar as missoes
+    @GetMapping("/listar") // quando voce quer dar para o usuario alguma coisa
+    public String listarMissoao(){
+        return "Missao listadas com sucesso!";
+    }
+    // Localhost:8080/missoes/criar
+    // postMapping utilizado quando o usuario quer mandar algo
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso!";
+    }
+
+    // put -- Mandar uma requisição para alterar missoes
+    @PutMapping("/alterar") // voce vai alterar o metodo que ja esta criado no seu banco de dados
+    public String alterarMissao(){
+        return "Missao alterada com sucesso!";
+    }
+
+    // Delete --  Mandar uma requisição para deletar as missoes
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso!";
+    }
+
+
 }
